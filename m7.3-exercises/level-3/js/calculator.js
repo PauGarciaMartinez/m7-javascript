8ded52fb8e0ae5c3742e686b78ef48ffcbc1052f
Pau Garcia Martínez <etpau@hotmail.es>
2021-01-24 02:09:19 +0100

    Add some funcionality

--- m7.3-exercises/level-3/js/calculator.js
+++ m7.3-exercises/level-3/js/calculator.js
@@ -1,3 +1,93 @@
+// GLOBAL VARIABLES 
+
+// To store selected numbers and stack them
+let number = "";
+let count = 0;
+
+// To check which operation it's been requested
+let addition = false
+let substraction = false;
+let multiplication = false;
+let division = false;
+
+// To check if it's the first operation
+let first = true;
+
+// DOM
+
+const screen = document.querySelector(".display-screen"); 
+const displayNumber = document.querySelectorAll('.num-btn').forEach(target => {
+    target.addEventListener('click', (e) => {
+        screen.textContent = number + target.value;
+        number = screen.textContent;
+    })
+  })
+const addOperator = document.getElementById("add");
+const substractOperator = document.getElementById("substract");
+const result = document.getElementById("equals");
+const clear = document.getElementById("clear");
+
+// OPERATION FUNCTIONS
+
+addOperator.addEventListener("click", (e) => {
+    if (substraction == true) {
+        count = count - number;
+        screen.textContent = count;
+    } else {
+        count = count + parseInt(number);
+        screen.textContent = count;
+    }
+    number = "";
+    addition = true;
+    substraction = false;
+    first = false;
+})
+
+substractOperator.addEventListener("click", (e) => {
+    if (first == true) {
+        if (addition == true) { //oju que si sumo abans aquí ho compta com a suma
+            count = count + parseInt(number);
+            screen.textContent = count;
+        } else {
+            count = count - number;
+            screen.textContent = count;
+        }
+    } else {
+        screen.textContent = count;
+        first = false;
+    }
+    number = "";
+    addition = false;
+    substraction = true;
+})
+
+// RESULT FUNCTION
+
+result.addEventListener("click", (e) => {
+    if (addition == true) {
+        screen.textContent = count + parseInt(number);
+    } else if (substraction == true) {
+        screen.textContent = count - number;
+    } else if (multiplication == true) {
+        screen.textContent = count;
+    } else if (division == true) {
+        screen.textContent = count;
+    }
+    count = parseInt(screen.textContent);
+    number = 0;
+})
+
+
+
+clear.addEventListener("click", (e) => {
+    number = 0;
+    count = 0;
+    screen.textContent = count;
+})
+
+
+
+
 // OPERATOR FUNCTIONS
 
 function add(value1, value2) {
@@ -36,10 +126,4 @@ function operate(operator, value1, value2) {
 console.log(operate("substract", 28, 6)); 
 
 
-// SET VALUES ON DISPLAY SCREEN CLICKING BUTTONS
 
-const screen = document.querySelector("display-screen");
-const btn = document.querySelector("#one");
-btn.addEventListener("click", () => {
-  alert("Hey");
-});
