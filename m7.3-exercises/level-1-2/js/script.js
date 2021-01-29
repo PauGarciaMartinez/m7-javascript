eab3f1241aba9602e00ce72b4af855d7cd1f10cf
Pau Garcia Martínez <etpau@hotmail.es>
2021-01-29 20:30:36 +0100

    Finish validations

---
+++ m7.3-exercises/level-1-2/js/script.js
@@ -0,0 +1,118 @@
+// DOM
+
+const search = document.getElementById("inputSearch");
+const email1 = document.getElementById("inputEmail");
+const password1 = document.getElementById("inputPassword");
+const password2 = document.getElementById("inputPassword2");
+const province = document.getElementById("inputSelect");
+const email2 = document.getElementById("inputEmail2");
+const password3 = document.getElementById("inputPassword3");
+
+const searchForm = document.getElementById("search-form");
+const registerForm = document.getElementById("register-form");
+const loginForm = document.getElementById("login-form");
+
+const emailRegex = /([a-zñ\d\.-]+)@([a-zñ\d\.-]+)\.([a-z]{2,8})/gi;
+const passwordRegex = /^(?=.*[ÑA-Z])(?=.*[0-9])(?=.{8,})/;
+
+// SUBMIT FUNCTIONS
+
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  checkInputsSearch();
+})
+
+registerForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  checkInputsRegister();
+})
+
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  checkInputsLogin();
+})
+
+// CHECK FUNCTIONS
+
+// Search
+function checkInputsSearch() {
+  const searchValue = search.value.trim();
+
+  if (searchValue.length < 3) {
+    setErrorFor(search, "Search must contain at least three characters");
+  } else {
+    setSuccessFor(search)
+  }
+}
+
+// Register
+function checkInputsRegister() {
+  const email1Value = email1.value.trim();
+  const password1Value = password1.value.trim();
+  const password2Value = password2.value.trim();
+  const provinceValue = province.value;
+
+  if (email1Value.match(emailRegex)) {
+    setSuccessFor(email1);
+  } else {
+    setErrorFor(email1, "Email is not valid");
+  }
+
+  if (password1Value === "") {
+    setErrorFor(password1, "Password can't be blank");
+  } else if (password1Value.match(passwordRegex)) {
+    setSuccessFor(password1);
+  } else {
+    setErrorFor(password1, "Password must contain at least eigth characters, including one uppercase and one digit");
+  }
+
+  if (password2Value !== password1Value) {
+    setErrorFor(password2, "Password does not match");
+  } else if (password2Value === "") {
+    setErrorFor(password2, "Password can't be blank");
+  } else {
+    setSuccessFor(password2);
+  }
+
+  if (provinceValue === "") {
+    setErrorFor(province, "Please select an option");
+  } else {
+    setSuccessFor(province);
+  }
+}
+
+// Login
+function checkInputsLogin() {
+  const email2Value = email2.value.trim();
+  const password3Value = password3.value.trim();
+
+  if (email2Value.match(emailRegex)) {
+    setSuccessFor(email2);
+  } else {
+    setErrorFor(email2, "Email is not valid");
+  }
+
+  if (password3Value === "") {
+    setErrorFor(password3, "Password can't be blank");
+  } else if (password3Value.length < 8) {
+    setErrorFor(password3, "Incorrect password");
+  } else {
+    setSuccessFor(password3);
+  }
+}
+
+// SUCESS OR ERROR FUNCTIONS 
+
+function setErrorFor(input, message) {
+  const formGroup = input.parentElement;
+  const small = formGroup.querySelector("small");
+  small.innerText = message;
+
+  formGroup.classList.add("error");
+}
+
+function setSuccessFor(input) {
+  const formGroup = input.parentElement;
+  formGroup.classList.remove("error");
+  formGroup.classList.add("sucess");
+}
